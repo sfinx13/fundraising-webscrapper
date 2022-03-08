fdc94e7caeefd9cd5ec8f4031b61c6025ce87560
saidi <saidi.ahamada@gmail.com>
2022-03-08 18:05:15 +0100

    fix wording

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from parameters import *
 from utils import *
 
-print('[start] Scrape data from cotizup website')
+print('[start] Extract data from website')
 projects = []
 for url in URLS_TO_SCRAP:
     projects += get_projects_from(url)
 
 projects = sorted(
     projects, key=lambda project: project['launched_at'], reverse=True)
-
 
 filename = generate_file_from(CSV_HEADER, projects)
 
